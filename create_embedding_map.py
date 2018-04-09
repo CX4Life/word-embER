0ea5fd5273b18a6ed043e2a105e438d788defc0e
Tim Woods <tim.woods@emergencyreporting.com>
2018-04-09 14:47:22 -0700

    Create string to embedding lookup

---
+++ create_embedding_map.py
@@ -0,0 +1,35 @@
+import json
+import numpy as np
+
+
+def load_rank_map():
+    with open('rank2word.json') as rank_map_file:
+        rank_map = json.load(rank_map_file)
+    return rank_map
+
+
+def load_embeddings_list():
+    embeddings = np.load('embeddings.npy')
+    print(embeddings.shape)
+    return embeddings
+
+
+def create_embedding_map(dictionary, embeddings):
+    word_embedding_map = {}
+    for rank, row in enumerate(embeddings):
+        word = dictionary[str(rank)]
+        print(word)
+        word_embedding_map[word] = row
+
+    return word_embedding_map
+
+
+def main():
+    ranks_to_words = load_rank_map()
+    embeddings = load_embeddings_list()
+    string_to_ndarray_map = create_embedding_map(ranks_to_words, embeddings)
+    np.save('embedding_lookup.npy', string_to_ndarray_map)
+
+
+if __name__ == '__main__':
+    main()
